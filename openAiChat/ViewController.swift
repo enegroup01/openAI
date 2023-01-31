bd8493de44901a09c486deeed52c7c8ed22159ff
ethan <enegroup01@gmail.com>
2023-01-31 16:34:02 +0800

    Initial Commit

---
+++ openAiChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  openAiChat
+//
+//  Created by ethan on 2023/1/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
